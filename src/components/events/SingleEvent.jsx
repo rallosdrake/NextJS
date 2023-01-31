ba637b97247c5bc3152087f1438410015cb9d814
James Barlow <98394470+rallosdrake@users.noreply.github.com>
2023-01-31 15:39:55 +0000

    adds email-regirstation route

--- src/components/events/SingleEvent.jsx
+++ src/components/events/SingleEvent.jsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useRouter } from "next/router";
 import Image from "next/image";
 export default function SingleEvent({ data }) {
-  const onClick = () => {
-    console.log("cklicked");
+  const inputEmail = useRef();
+
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const emailValue = inputEmail.current.value;
+    const eventId = router.query.id;
+
+    try {
+      const response = await fetch(`/api/email-registered`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: emailValue, eventId }),
+      });
+      if (!response.ok) throw new Error("Something went wrong");
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
-    <h1>
-      <div>
-        <Image src={data.image} width={500} height={300} alt={data.title} />
-        <h1>{data.title}</h1>
-        <p>{data.description}</p>
-        <input type="email" />
-        <button onClick={onClick}>Submit</button>
-      </div>
-    </h1>
+    <div className="event-single-page">
+      <Image src={data.image} width={500} height={300} alt={data.title} />
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+      <form onSubmit={onSubmit} className="email-registration">
+        <input
+          ref={inputEmail}
+          type="email"
+          id="email"
+          placeholder="Sign up here"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
